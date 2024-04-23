994c881411e679ae85f3752c6cda9f5b0b5cbcf7
Dear <lechadoq@gmail.com>
2024-04-23 22:34:33 +0200

    Add swabian pulse streamer

---
+++ caqtus_devices.swabian_pulse_streamer/caqtus_devices/swabian_pulse_streamer/configuration_editor/__init__.py
@@ -0,0 +1,9 @@
+from swabian_pulse_streamer.configuration import SwabianPulseStreamerConfiguration
+from .swabian_pulse_streamer_configuration_editor import (
+    SwabianPulseStreamerDeviceConfigEditor,
+)
+
+__all__ = [
+    "SwabianPulseStreamerDeviceConfigEditor",
+    "SwabianPulseStreamerConfiguration",
+]
