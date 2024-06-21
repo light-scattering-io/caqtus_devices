5c87d2916e301c50a5d1a4192b5004b0224504e7
Dear <lechadoq@gmail.com>
2024-06-21 15:54:45 +0200

    Add methods to update channel state

--- caqtus_devices.siglent_sdg6022x/caqtus_devices/siglent_sdg6022x/_runtime.py
+++ caqtus_devices.siglent_sdg6022x/caqtus_devices/siglent_sdg6022x/_runtime.py
@@ -1,12 +1,54 @@
+import abc
 import contextlib
 import logging
-from typing import Self
+import time
+from typing import Self, Literal
 
+import attrs
 import pyvisa
 import pyvisa.constants
 from caqtus.device import Device
 
 logger = logging.getLogger(__name__)
+
+
+@attrs.define
+class ChannelState(abc.ABC):
+    channel: int
+    output_enabled: bool = attrs.field(validator=attrs.validators.instance_of(bool))
+    load: float | Literal["High Z"] = attrs.field(
+        validator=attrs.validators.in_([50, "High Z"])
+    )
+
+    def apply(self, instr: pyvisa.resources.Resource) -> None:
+        self.set_output(instr)
+
+    def prefix(self) -> str:
+        return f"C{self.channel + 1}:"
+
+    def set_output(self, instr: pyvisa.resources.Resource) -> None:
+        load = "HZ" if self.load == "High Z" else f"{self.load}"
+        polarity = "NORM"
+        output = "ON" if self.output_enabled else "OFF"
+        command = f"{self.prefix()}OUTP {output},LOAD,{load},PLRT,{polarity}"
+        logger.debug("Sending command %r", command)
+        instr.write(command)
+
+
+@attrs.define
+class SiglentState:
+    """State of a Siglent SDG6022X arbitrary waveform generator."""
+
+    channel_0: ChannelState = attrs.field(
+        validator=attrs.validators.instance_of(ChannelState)
+    )
+    channel_1: ChannelState = attrs.field(
+        validator=attrs.validators.instance_of(ChannelState)
+    )
+
+    def apply(self, instr: pyvisa.resources.Resource) -> None:
+        self.channel_0.apply(instr)
+        self.channel_1.apply(instr)
 
 
 class SiglentSDG6022X(Device):
@@ -46,11 +88,24 @@ class SiglentSDG6022X(Device):
             raise
         return self
 
+    def update_state(self, state: SiglentState) -> None:
+        t0 = time.perf_counter()
+        state.apply(self._instr)
+        t1 = time.perf_counter()
+        logger.info("Updated to state %s in %.3f s", state, t1 - t0)
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         return self._exit_stack.__exit__(exc_type, exc_val, exc_tb)
+
+    def write_command(self, command: str) -> None:
+        self._instr.write(command)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     with SiglentSDG6022X("TCPIP0::192.168.137.164::inst0::INSTR") as siglent:
-        pass
+        state = SiglentState(
+            channel_0=ChannelState(channel=0, output_enabled=True, load="High Z"),
+            channel_1=ChannelState(channel=1, output_enabled=True, load=50),
+        )
+        siglent.update_state(state)
