c7f5f49768823a9a7c02521dad6a203ab755dd38
damien <damien.bloch@institutoptique.fr>
2024-07-22 22:19:26 +0200

    Update imaging source configuration editor to use form camera configuration editor

--- caqtus_devices.imaging_source/caqtus_devices/imaging_source/configuration_editor/editor.py
+++ caqtus_devices.imaging_source/caqtus_devices/imaging_source/configuration_editor/editor.py
@@ -1,7 +1,8 @@
-from PySide6.QtWidgets import QLineEdit, QComboBox
-from caqtus.utils.roi import RectangularROI, Width, Height
+from typing import Optional
 
-from caqtus.gui.condetrol.device_configuration_editors.camera_configuration_editor.editor import (
+from PySide6.QtWidgets import QLineEdit, QComboBox, QWidget
+
+from caqtus.gui.condetrol.device_configuration_editors.camera_configuration_editor import (
     CameraConfigurationEditor,
 )
 from ..configuration import ImagingSourceCameraConfiguration
@@ -12,44 +13,21 @@ class ImagingSourceCameraConfigurationEditor(
 ):
     def __init__(
         self,
-        *args,
-        **kwargs,
+        configuration: ImagingSourceCameraConfiguration,
+        parent: Optional[QWidget] = None,
     ):
-        super().__init__(*args, **kwargs)
+        super().__init__(configuration, parent)
 
         self._camera_name = QLineEdit()
-        self.form.insertRow(0, "Camera name", self._camera_name)
+        self._camera_name.setText(configuration.camera_name)
+        self.insert_row("Camera name", self._camera_name, 1)
         self._format_combo_box = QComboBox()
         self._format_combo_box.addItems(["Y800", "Y16"])
-        self.form.insertRow(1, "Format", self._format_combo_box)
-        self.update_ui_from_config(self.device_config)
+        self._format_combo_box.setCurrentText(configuration.format)
+        self.insert_row("Format", self._format_combo_box, 2)
 
-    def update_config_from_ui(
-        self, device_config: ImagingSourceCameraConfiguration
-    ) -> ImagingSourceCameraConfiguration:
-        device_config = super().update_config_from_ui(device_config)
-        device_config.camera_name = self._camera_name.text()
-        device_config.format = self._format_combo_box.currentText()
-        return device_config
-
-    def update_ui_from_config(
-        self, device_config: ImagingSourceCameraConfiguration
-    ) -> None:
-        super().update_ui_from_config(device_config)
-        self._camera_name.setText(device_config.camera_name)
-        self._format_combo_box.setCurrentText(device_config.format)
-
-    @classmethod
-    def get_default_configuration(cls) -> ImagingSourceCameraConfiguration:
-        return ImagingSourceCameraConfiguration(
-            camera_name="",
-            remote_server="default",
-            format="Y800",
-            roi=RectangularROI(
-                original_image_size=(Width(1280), Height(960)),
-                x=0,
-                y=0,
-                width=1280,
-                height=960,
-            ),
-        )
+    def get_configuration(self) -> ImagingSourceCameraConfiguration:
+        configuration = super().get_configuration()
+        configuration.camera_name = self._camera_name.text()
+        configuration.format = self._format_combo_box.currentText()
+        return configuration
