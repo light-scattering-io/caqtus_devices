5a5de68d9ae03fe7d171f33eacbfbf5c5d3922d2
damien <damien.bloch@institutoptique.fr>
2024-10-27 13:02:35 +0100

    Make caqtus.gui.autogen public

--- caqtus_devices.orca_quest/src/caqtus_devices/orca_quest/configuration_editor/orca_quest_config_editor.py
+++ caqtus_devices.orca_quest/src/caqtus_devices/orca_quest/configuration_editor/orca_quest_config_editor.py
@@ -1,6 +1,4 @@
-from caqtus.gui.condetrol.device_configuration_editors._autogen import (
-    build_device_configuration_editor,
-)
+from caqtus.gui.autogen import build_device_configuration_editor
 from ..configuration import OrcaQuestCameraConfiguration
 
 OrcaQuestConfigurationEditor = build_device_configuration_editor(
